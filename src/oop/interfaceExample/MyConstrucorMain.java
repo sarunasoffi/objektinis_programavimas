5b40e5e663432577fbb51f357f0cf3ea803a41b7
unknown <sarunas.s.1993@gmail.com>
2018-03-16 16:30:21 +0200

    ketvirtas commitas

--- src/oop/interfaceExample/MyConstrucorMain.java
+++ src/oop/interfaceExample/MyConstrucorMain.java
@@ -4,7 +4,9 @@ public class MyConstrucorMain {
 
     public static void main(String[] args) {
 
+        Student st = new Student(24, "Sarunas", "Sliogeris");
 
+        System.out.println(st.getName() + " " + st.getSurname() + " " + st.getAge() + " metai");
 
     }
 
